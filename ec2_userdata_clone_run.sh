f86919657532571b109e13c63c3ac48a94fea417
Nagaraj-koti <hubliray@yahoo.com>
2025-09-17 14:18:54 +0530

    crontab added in userdata

--- ec2_userdata_clone_run.sh
+++ ec2_userdata_clone_run.sh
@@ -16,6 +16,8 @@ apt-get install -y git
 GITHUB_REPO_URL="https://github.com/nagarajAI/real_scripts.git"
 TARGET_DIR="/opt/myapp"
 SCRIPT_TO_RUN="apt_jenkins_java.sh"   # path inside repo
+CRON_SCRIPT="jenkins_publicIP_update.sh"         # script to run via cron
+CRON_SCHEDULE="@reboot"               # schedule.
 # -------------------------------------------
 
 echo "Cloning repository..."
@@ -29,5 +31,19 @@ chmod +x "$SCRIPT_TO_RUN"
 
 echo "Running the script..."
 ./"$SCRIPT_TO_RUN"
+
+
+# ----- Add cron job -----
+echo "Scheduling cron job..."
+
+# For the 'ubuntu' default user:
+# Create/append to their crontab. Ensure correct path to script and environment.
+CRON_LINE="$CRON_SCHEDULE /bin/bash $TARGET_DIR/$CRON_SCRIPT >> /var/log/backup.log 2>&1"
+# Add only if not already present
+(crontab -u ubuntu -l 2>/dev/null; echo "$CRON_LINE") | crontab -u ubuntu -
+
+# Start cron service (Ubuntu’s package installs it but we ensure it’s enabled)
+systemctl enable cron
+systemctl start cron
 
 echo "User-data script completed."
